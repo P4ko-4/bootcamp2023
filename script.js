5fe0e5806170d48e8cf9172a01766684e5dc7a25
P4ko-4 <pakoledi@hotmail.com>
2023-03-02 13:15:36 +0200

    Task3

---
+++ script.js
@@ -0,0 +1,14 @@
+// window.onload = function () {
+    
+// //Home Page Variables
+// let homeTitle = "Pako - Aspiring Developer"
+// document.getElementById('h_title').innerHTML = homeTitle;
+
+// let expList = ["Daily I'd assist fellow employees with network, hardware & software related queries."  
+//             + '<br><br>' + "Duties mentioned above were also performed telephonically by guiding users on how to resolve"
+//        + "their respective queries"];
+//        document.getElementById('workExp').innerHTML = expList;
+
+// }
+
+
